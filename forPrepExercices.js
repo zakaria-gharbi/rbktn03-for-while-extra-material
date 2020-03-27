44729c9355f1cda7107ca98f8f318b5fac2dea56
zakaria.gharbi <hagerbenghdir@gmail.com>
2020-03-27 17:43:16 +0100

    zico

--- forPrepExercices.js
+++ forPrepExercices.js
@@ -2,9 +2,17 @@
 // 0.Try to write all of the exercises using both the for loop and while loop.
 
 // 1.Write a function sum that computes the sum of the numbers in an array.
-
+   function sum(arr[]){
+    var sum =0;
+   for(var i =0;i< arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum;
+   }
 // 2.Write a function max that accepts an array of numbers and returns the largest number in the array.
-
+ function max(){
+ 	for(var i =0;)
+ }
 // 3.Try the following at a console:
 
 //  'the quick brown fox jumped over the lazy dog'.split(' ');
